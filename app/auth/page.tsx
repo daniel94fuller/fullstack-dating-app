556f75306a1e6845775f7283f6b9b52e2213c965
daniel94fuller <daniel@popcircle.com>
2026-04-30 23:15:26 -0700

    fix auth

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,4 +1,6 @@
-"use client";
+export const dynamic = "force-dynamic"; // 🔥 ADD THIS LINE
+
+("use client");
 
 import Logo from "@/components/Logo";
 import { useAuth } from "@/contexts/auth-context";
